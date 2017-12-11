dff8c0fad9fa6c7e98515e58146187125d10cff8
SerjKrukov <serj.krukov@gmail.com>
2017-12-11 23:19:29 +0200

    deployment fix

--- js/server.js
+++ js/server.js
@@ -7,7 +7,7 @@ const socketIO = require('socket.io');
 const path = require('path');
 
 const PORT = process.env.PORT || 5001;
-const INDEX = path.join(__dirname, 'index.html');
+const INDEX = path.join(__dirname,'../' , 'index.html');
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
