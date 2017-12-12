7bf4617f4b2be333369688fa0d985623c7f18bdf
SerjKrukov <serj.krukov@gmail.com>
2017-12-12 02:02:55 +0200

    dead code delete

--- public/js/app2.js
+++ public/js/app2.js
@@ -7,17 +7,12 @@ function handleFile(e) {
   reader.onload = function(e) {
     out.innerHTML = '';
     var data = e.target.result;
-    console.log(data);
     if(!rABS) data = new Uint8Array(data);
     var workbook = XLSX.read(data, {type: rABS ? 'binary' : 'array'});
-
-    var worksheet = workbook.Sheets[workbook.SheetNames[0]];
-    console.log(worksheet);
     workbook.SheetNames.forEach(function(sheetName) {
       out.innerHTML += XLSX.utils.sheet_to_html(workbook.Sheets[sheetName])
     });
     out.innerHTML = XLSX.utils.sheet_to_html(worksheet);
-    /* DO SOMETHING WITH workbook HERE */
   };
   if(rABS) reader.readAsBinaryString(f); else reader.readAsArrayBuffer(f);
 }
